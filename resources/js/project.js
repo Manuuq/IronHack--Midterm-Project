9e966388b6d4b3a121022807de04fb5198ae6d6f
Manuel Quintana <quintana.manuel93@example.com>
2023-01-27 15:13:20 +0100

    Homepage actualizacion

--- resources/js/project.js
+++ resources/js/project.js
@@ -1,2 +1,20 @@
-/* Crea tu propia lógica para hacer fetch de un post y enseñar su información utilizando DOM manipulation */
-/* ADVANCED: consigue que la info del post funcione dinámicamente y enseñe un post u otro según la URL */
+let projNum = parseInt(window.location.search.split('?')[1]);
+let tituloName;
+let subtituloName;
+let fotoName;
+let paragraphName;
+let placeholder = fetch('https://jsonplaceholder.typicode.com/posts/1/comments?_start=0&_limit=4')
+.then((response) => response.json())
+.then((cleanResponse) => {
+    console.log(cleanResponse[0].name);
+            tituloName = cleanResponse[0].name;
+            subtituloName = cleanResponse[0].email;
+            fotoName = cleanResponse[0].id;
+            paragraphName = cleanResponse[0].body;
+        document.getElementById("project-titulo").innerHTML = tituloName;
+        document.getElementById("subtitle-description").innerHTML = subtituloName;
+        document.querySelector('.project-intro-image').src = `/project-assets/projects-section/${fotoName}.jpg`;
+        document.querySelector('.project-intro-image-effect').src = `/project-assets/projects-section/${fotoName}.jpg`;
+        document.getElementById("project-details-p").innerHTML = paragraphName;
+    }
+);
