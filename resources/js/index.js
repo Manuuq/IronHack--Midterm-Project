d4fb1bf5eb0ba42fc695e96e3d8c8d25ef9e4715
Manuel Quintana <quintana.manuel93@example.com>
2023-02-22 18:42:18 +0100

    midterm actualizado

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,72 @@
-/* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+let projectArray = document.querySelectorAll(".project");
+let projectData = fetch("https://jsonplaceholder.typicode.com/posts")
+.then((res) => res.json()).then((res) => {
+    projectArray[0].querySelector("h3").innerText = res[0].title;
+    projectArray[0].querySelector("p").innerText = res[0].body;
+    projectArray[0].querySelector("a").href = "./project.html?slug=" + slugify( res[0].title);
+
+    projectArray[1].querySelector("h3").innerText = res[1].title;
+    projectArray[1].querySelector("p").innerText = res[1].body;
+    projectArray[1].querySelector("a").href = "./project.html?slug=" + slugify(res[1].title);
+
+    projectArray[2].querySelector("h3").innerText = res[2].title;
+    projectArray[2].querySelector("p").innerText = res[2].body;
+    projectArray[2].querySelector("a").href = "./project.html?slug=" + slugify(res[2].title);
+
+}).catch((err) => console.log(err))
+
+let scrollBtn = document.querySelector(".scroll");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollBtn.style.display = "block";
+    } else {
+      scrollBtn.style.display = "none";
+    }
+  }
+
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+scrollBtn.addEventListener("click", scrollToTop);
+
+let scrollToProject = document.querySelector(".scrollToProject");
+let scrollToProjectHam = document.querySelector(".ham-scroll-project");
+let elementToScroll = document.querySelector(".projects")
+
+const scrollToProjectFunction = () => {
+    elementToScroll.scrollIntoView({behavior:"smooth"})
+}
+
+scrollToProject.addEventListener("click", scrollToProjectFunction);
+scrollToProjectHam.addEventListener("click", scrollToProjectFunction);
+
+
+let scrollToServices = document.querySelector(".scrollToServices");
+let scrollToServicesHam = document.querySelector(".ham-scroll-services");
+let elementToScrollService = document.querySelector(".our-Services")
+
+const scrollToServicesFunction = () => {
+    elementToScrollService.scrollIntoView({behavior:"smooth"})
+}
+
+scrollToServices.addEventListener("click", scrollToServicesFunction);
+scrollToServicesHam.addEventListener("click", scrollToServicesFunction);
+
+
+
+function slugify(text) {
+    return text.toString().toLowerCase().trim()
+      .normalize('NFD') 				 // separate accent from letter
+      .replace(/[\u0300-\u036f]/g, '') // remove all separated accents
+      .replace(/\s+/g, '-')            // replace spaces with -
+      .replace(/&/g, '-and-')          // replace & with 'and'
+      .replace(/[^\w\-]+/g, '')        // remove all non-word chars
+      .replace(/\-\-+/g, '-')          // replace multiple '-' with single '-'
+  }
--- resources/js/project.js
+++ resources/js/project.js
@@ -1,4 +1,4 @@
-let projNum = parseInt(window.location.search.split('?')[1]);
+/* let projNum = parseInt(window.location.search.split('?')[1]);
 let tituloName;
 let subtituloName;
 let fotoName;
@@ -17,4 +17,96 @@ let placeholder = fetch('https://jsonplaceholder.typicode.com/posts/1/comments?_
         document.querySelector('.project-intro-image-effect').src = `/project-assets/projects-section/${fotoName}.jpg`;
         document.getElementById("project-details-p").innerHTML = paragraphName;
     }
-);
+); */
+
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+const param = urlParams.get("slug");
+
+
+let projectArray = document.querySelectorAll(".project");
+let documentProject = document.querySelector(".main-project")
+let img = documentProject.appendChild(document.createElement("img"));
+let paragraf = documentProject.appendChild(document.createElement("p"));
+
+let projectData = fetch("https://jsonplaceholder.typicode.com/posts")
+.then((res) => res.json()).then((res) => {
+    projectArray[0].querySelector("h3").innerText = res[0].title;
+    projectArray[0].querySelector("p").innerText = res[0].body.replace(/(\r\n|\n|\r)/gm, "");
+    projectArray[0].querySelector("a").href = "./project.html?slug=" + slugify( res[0].title);
+
+    projectArray[1].querySelector("h3").innerText = res[1].title;
+    projectArray[1].querySelector("p").innerText = res[1].body.replace(/(\r\n|\n|\r)/gm, "");
+    projectArray[1].querySelector("a").href = "./project.html?slug=" + slugify( res[1].title);
+
+    projectArray[2].querySelector("h3").innerText = res[2].title;
+    projectArray[2].querySelector("p").innerText = res[2].body.replace(/(\r\n|\n|\r)/gm, "");
+    projectArray[2].querySelector("a").href = "./project.html?slug=" + slugify( res[2].title);
+
+    
+    let found = res.find(element => slugify(element.title) === param);
+    console.log(found);
+    if (found) {
+        let documentProject = document.querySelector(".main-project")
+        documentProject.querySelector("h1").innerText = found.title;
+        documentProject.querySelector("p").innerText = found.body.replace("\n"," ");
+        if (found.title.includes("facere")) {
+            img.setAttribute("src", "./resources/images/projects-section/1.jpg");
+            img.classList.add("project-image")
+            let text = found.body.replace(/(\r\n|\n|\r)/gm, "")
+            console.log(text);
+            paragraf.innerText = text;
+            paragraf.classList.add("project-paragraf")
+            
+        } else if (found.title.includes("esse")) {
+            img.setAttribute("src", "./resources/images/projects-section/2.jpg");
+            img.classList.add("project-image")
+            paragraf.innerText = found.body.replace(/(\r\n|\n|\r)/gm, "")
+            paragraf.classList.add("project-paragraf")
+            console.log(paragraf.innerText);
+        } else if (found.title.includes("molestias")){
+            img.setAttribute("src", "./resources/images/projects-section/3.jpg");
+            img.classList.add("project-image")
+            paragraf.innerText = found.body.replace(/(\r\n|\n|\r)/gm, "")
+            paragraf.classList.add("project-paragraf")
+            
+        }
+
+    } else {
+        let documentProject = document.querySelector(".main-project")
+        documentProject.querySelector("h1").innerText = res[0].title;
+        documentProject.querySelector("p").innerText = res[0].body.replace("\n"," ");
+        img.setAttribute("src", "./resources/images/projects-section/1.jpg");
+        img.classList.add("project-image")
+        let text = res[0].body.replace(/(\r\n|\n|\r)/gm, "")
+        console.log(text);
+        paragraf.innerText = text;
+        paragraf.classList.add("project-paragraf")
+    }
+
+}).catch((err) => console.log(err))
+
+
+let scrollToProject = document.querySelector(".scrollToProject");
+let scrollToProjectHam = document.querySelector(".ham-scroll-project");
+let elementToScroll = document.querySelector(".projects")
+
+const scrollToProjectFunction = () => {
+    elementToScroll.scrollIntoView({behavior:"smooth"})
+}
+
+scrollToProject.addEventListener("click", scrollToProjectFunction);
+scrollToProjectHam.addEventListener("click", scrollToProjectFunction);
+
+
+function slugify(text) {
+    return text.toString().toLowerCase().trim()
+      .normalize('NFD') 				 // separate accent from letter
+      .replace(/[\u0300-\u036f]/g, '') // remove all separated accents
+      .replace(/\s+/g, '-')            // replace spaces with -
+      .replace(/&/g, '-and-')          // replace & with 'and'
+      .replace(/[^\w\-]+/g, '')        // remove all non-word chars
+      .replace(/\-\-+/g, '-')          // replace multiple '-' with single '-'
+  }
